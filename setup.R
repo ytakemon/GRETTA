c3115a05499f21b45b52e4d8d6049d4055c4c101
Richard Corbett <rcorbett@bcgsc.ca>
2024-12-10 14:21:08 -0800

    Remove references to old branch

--- setup.R
+++ setup.R
@@ -1,7 +1,7 @@
 library(devtools)
 
 # Install the GitHub package
-devtools::install_github("Marralab/GRETTA")# ref="tire_kicking", force = TRUE)
+devtools::install_github("Marralab/GRETTA")
 
 # Test the package
 library(GRETTA)
